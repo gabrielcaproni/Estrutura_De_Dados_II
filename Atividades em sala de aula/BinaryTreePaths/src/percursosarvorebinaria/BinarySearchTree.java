a2f3a3c0b6cab7baaf9eb5eb04ce7254ba27f759
gabrielcaproni <gabrielcaproni09@gmail.com>
2023-10-05 08:23:26 -0300

    Arvores binarias.

--- Atividades em sala de aula/BinaryTreePaths/src/percursosarvorebinaria/BinarySearchTree.java
+++ Atividades em sala de aula/BinaryTreePaths/src/percursosarvorebinaria/BinarySearchTree.java
@@ -8,12 +8,60 @@ package percursosarvorebinaria;
 public class BinarySearchTree<T extends Comparable<T>>{
     Node raiz = null;
     
-    public void add(T novoNode){
-        Node<T> novoNo = new Node<T>(novoNode);
-        raiz = inserir(raiz, novoNo);
+    public void add(T newNode){
+        Node<T> newNo = new Node<T>(newNode);
+        raiz = inserir(raiz, newNo);
     }
 
-    private Node<T> inserir(Node<T> raiz, Node<T> novoNode) {
+    private Node<T> inserir(Node<T> raiz, Node<T> newNode) {
         
+        if(raiz == null){
+            return newNode;
+        }
+        if(newNode.dado.compareTo(raiz.dado) >= 0){
+            raiz.direita = inserir(raiz.direita, newNode);
+        }else{
+            raiz.esquerda = inserir(raiz.esquerda, newNode);
+        }
+        
+        return raiz;
+    }
+    
+    public void preOrder(){
+        preOrder(raiz);
+    }
+    
+    private void preOrder(Node<T> raiz){
+        
+        if(raiz != null){
+            System.out.print(raiz.dado + " ");
+            preOrder(raiz.esquerda);
+            preOrder(raiz.direita);
+        }
+    }
+    
+    public void inOrder(){
+        inOrder(raiz);
+    }
+    
+    private void inOrder(Node<T> raiz){
+        if(raiz != null){
+            inOrder(raiz.esquerda);
+            System.out.print(raiz.dado + " ");
+            inOrder(raiz.direita);
+        }
+    }
+    
+    public void posOrder(){
+        posOrder(raiz);
+    }
+    
+    private void posOrder(Node<T> raiz){
+        
+        if(raiz != null){
+            posOrder(raiz.esquerda);
+            posOrder(raiz.direita);
+            System.out.print(raiz.dado + " ");
+        }
     }
 }
--- Atividades em sala de aula/BinaryTreePaths/src/percursosarvorebinaria/BinaryTreePaths.java
+++ Atividades em sala de aula/BinaryTreePaths/src/percursosarvorebinaria/BinaryTreePaths.java
@@ -7,7 +7,26 @@ package percursosarvorebinaria;
  */
 public class BinaryTreePaths {
     public static void main(String[] args) {
-        // TODO code application logic here
+        BinarySearchTree tree = new BinarySearchTree();
+        
+        tree.add(new Aluno(30, "Athos"));
+        tree.add(new Aluno(15, "Ryan"));
+        tree.add(new Aluno(50, "Luiz"));
+        tree.add(new Aluno(40, "Guilherme"));
+        tree.add(new Aluno(45, "Andre"));
+        
+//        tree.add(30);
+//        tree.add(15);
+//        tree.add(50);
+//        tree.add(40);
+//        tree.add(45);
+        
+        System.out.println("Pre ordem");
+        tree.preOrder();
+        System.out.println("\nEm ordem");
+        tree.inOrder();
+        System.out.println("\nPos ordem");
+        tree.posOrder();
     }
     
 }
