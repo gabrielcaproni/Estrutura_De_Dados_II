d491e4d81d7b0fd2b4bfa15954dd17ca25effbfa
gabrielcaproni <gabrielcaproni09@gmail.com>
2023-11-29 09:40:14 -0300

    Busca em largura por grafos.

---
+++ Atividades em sala de aula/JavaTreeAVL/src/javatreesaula/Node.java
@@ -0,0 +1,16 @@
+
+package javatreesaula;
+
+public class Node<T extends Comparable<T>>{
+    T dado;
+    Node<T> esquerda;
+    Node<T> direita;
+    int fb;
+    
+    public Node(T novoDado){
+        this.dado = novoDado;
+        this.esquerda = null;
+        this.direita = null;
+        this.fb = 0;
+    }
+}
